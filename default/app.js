fd8e59e657f077dbdd4d3dfaf1374e5608cabe4b
Sergio Deras AT <sergio.deras@agilethought.com>
2021-05-05 13:47:58 -0500

    Functional

--- default/app.js
+++ default/app.js
@@ -1,53 +1,25 @@
-// Copyright 2018-2020Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.TABLE_NAME;
+const params = {
+  TableName: tableName,
+};
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
-
-const { TABLE_NAME } = process.env;
 
 exports.handler = async event => {
+  var scanResults = [];
+  var items;
+  do {
+    items =  await docClient.scan(params).promise();
+    items.Items.forEach((item) => scanResults.push(item));
+    params.ExclusiveStartKey  = items.LastEvaluatedKey;
+  } while(typeof items.LastEvaluatedKey !== "undefined");
+
+  var random = scanResults[Math.floor(Math.random() * scanResults.length)];
+  console.log(random)
   return { 
     statusCode: 200,
     headers: {},
-    body: JSON.stringify({"message": "OK"})
+    body: JSON.stringify(random.word)
   };
-      /*
-  let connectionData;
-  
-  try {
-    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
-  } catch (e) {
-    return { statusCode: 500, body: e.stack };
-  }
-  
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
-  });
-  
-  const postData = JSON.parse(event.body).data;
-  
-  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
-    try {
-      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
-    } catch (e) {
-      if (e.statusCode === 410) {
-        console.log(`Found stale connection, deleting ${connectionId}`);
-        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
-      } else {
-        throw e;
-      }
-    }
-  });
-  
-  try {
-    await Promise.all(postCalls);
-  } catch (e) {
-    return { statusCode: 500, body: e.stack };
-  }
-
-  return { statusCode: 200, body: 'Data sent.' };
-*/
 };
